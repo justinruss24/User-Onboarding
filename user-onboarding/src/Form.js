4753a5107bc321da2560b6b1865da8a1e7d81c4e
Justin Russell <Justin@Justins-MacBook.local>
2020-02-27 15:25:50 -0800

    some stretch

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -2,32 +2,54 @@ import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
 import * as Yup from "yup";
 import axios from "axios";
+import "./App.css";
 
 const UserForm = ({values, touched, errors, status}) => {
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        console.log("user status changed!", status);
+        status && setUser(user => [...user, status]);
+    }, [status]);
     return (
-        <div>
-            <Form>
-                <label htmlFor="name">
-                    Name
-                    <Field id="name" type="text" name="name" placeholder="name" />
-                    {touched.name && errors.name && (<p>{errors.name}</p>)}
-                </label>
-                <label htmlFor="email">
-                    Email
-                    <Field id="email" type="text" name="email" placeholder="your@email.here" />
-                    {touched.email && errors.email && (<p>{errors.email}</p>)}
-                </label>
-                <label htmlFor="password">
-                    Password
-                    <Field id="password" type="password" name="password" placeholder="password" />
-                    {touched.password && errors.password && (<p>{errors.password}</p>)}
-                </label>
-                <label htmlFor="terms">
-                    Do you agree to our Terms and Services?
-                    <Field id="terms" type="checkbox" name="terms" />
-                </label>
-                <button type="submit">Submit</button>
-            </Form>
+        <div className="container">
+            <div className="onboard">
+                <Form>
+                    <label htmlFor="name">
+                        Name
+                        <Field id="name" type="text" name="name" placeholder="name" />
+                        {touched.name && errors.name && (<p className="red">{errors.name}</p>)}
+                    </label>
+                    <br />
+                    <label htmlFor="email">
+                        Email
+                        <Field id="email" type="text" name="email" placeholder="your@email.here" />
+                        {touched.email && errors.email && (<p className="red">{errors.email}</p>)}
+                    </label>
+                    <br />
+                    <label htmlFor="password">
+                        Password
+                        <Field id="password" type="password" name="password" placeholder="password" />
+                        {touched.password && errors.password && (<p className="red">{errors.password}</p>)}
+                    </label>
+                    <br />
+                    <label htmlFor="terms">
+                        Do you agree to our Terms and Services?
+                        <Field id="terms" type="checkbox" name="terms" />
+                        {touched.terms && errors.terms && (<p className="red">You must agree to our terms.</p>)}
+                    </label>
+                    <br />
+                    <button type="submit">Submit</button>
+                </Form>
+            </div>
+            <div className="userList">
+                    {user.map(users => (
+                        <ul>
+                            <li> name: {users.name}</li>
+                            <li> email: {users.email}</li>
+                            <li> password: {users.password}</li>
+                        </ul>
+                    ))}
+            </div>
         </div>
     );
 };
@@ -45,7 +67,7 @@ const FormikUserForm = withFormik({
         name: Yup.string().required(),
         email: Yup.string().required(),
         password: Yup.string().min(5, 'Must be at least 5 characters').required(),
-        terms: Yup.boolean().oneOf([true]).required('You must agree to our terms.')
+        terms: Yup.boolean().oneOf([true]).required()
 
     }),
     handleSubmit(values, {setStatus, resetForm}) {
@@ -57,4 +79,6 @@ const FormikUserForm = withFormik({
             resetForm();
         });
     }
-})(UserForm)
+})(UserForm);
+
+export default FormikUserForm;
